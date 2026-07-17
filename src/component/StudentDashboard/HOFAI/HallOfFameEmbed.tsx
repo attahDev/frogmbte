cd49ffd4da32fe3610e02d630bf32fc462b75f9b
attahDev <attahumana007@gmail.com>
2026-07-17 10:35:57 +0100

    Add HallOfFameEmbed component with iframe

---
+++ src/component/StudentDashboard/HOFAI/HallOfFameEmbed.tsx
@@ -0,0 +1,12 @@
+const HOF_APP_URL = "https://hof-ochre.vercel.app";
+
+export default function HallOfFameEmbed() {
+  return (
+    <iframe
+      src={HOF_APP_URL}
+      title="Hall of Fame"
+      className="h-[calc(100dvh-97px)] min-h-[500px] w-full border-0"
+      allow="clipboard-write"
+    />
+  );
+}
