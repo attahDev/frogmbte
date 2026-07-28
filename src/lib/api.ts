b5fba3682b031c7dc71fa8a50d6d6ba2f2f7b0f8
attahDev <attahumana007@gmail.com>
2026-07-28 22:38:52 +0100

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://backgmb.onrender.com",
+  baseURL: "https://gmbtebac.onrender.com",
   withCredentials: true,
 });
 
